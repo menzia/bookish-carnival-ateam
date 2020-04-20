64e7ab691ed48cd15dadf150a7d72667acb7ada5
linyi98 <llyu4@wisc.edu>
2020-04-20 13:54:29 -0500

    The template for project MilkWeights System

---
+++ README.txt
@@ -0,0 +1,21 @@
+README
+
+Course: cs400
+Semester: Fall 2019
+Project name: Social Network
+Team Members:
+1. Alex Menzia, 001, and menzia@wisc.edu
+2. Ethan Huang, 002, and ihuang22@wisc.edu
+3. Linyi Lyu, 001, and llyu4@wisc.edu
+
+
+ 
+
+Which team members were on same xteam together?
+Alex Menzia and Linyi Lyu were in xteam179.
+
+Other notes or comments to the grader:
+
+[place any comments or notes that will help the grader here]
+
+ 
---
+++ application/Main.java
@@ -0,0 +1,51 @@
+/**
+ * Main.java created by {user} on Surface Pro 7 in ateam_MilkWeights
+ * Author:   Linyi Lyu (llyu4@wisc.edu)
+ * Date:     @date
+ * 
+ * Course:   CS400
+ * Semester: Spring 2020 
+ * Lecture:  001
+ * 
+ * IDE:      Eclipse IDE for Java Developers
+ * Version:  2019-12
+ * Build id:
+ * 
+ * Device:
+ * OS:
+ * Version:
+ * OS Build:
+ *
+ * List Collaborators: name, wisc.edu, lecture number
+ * 
+ * Other Credits: Other source, website or people
+ *
+ * Know Bugs: known unresolved bugs
+ */
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main - TODO Describe purpose of this user defined type
+ * @author Linyi Lyu (2020)
+ *
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
